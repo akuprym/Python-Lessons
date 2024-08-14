642eea58b08879f9660d76bbe7601dd31ddf05aa
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-08-14 23:09:26 +0300

    Methods with Lists

---
+++ _07_lists/lesson.py
@@ -0,0 +1,11 @@
+word = "49498588585"
+my_list = list(word)
+print(my_list)
+
+mylist = ["anna", "25", "minsk"]
+mystring = " ".join(mylist)
+
+print(mystring)
+
+newlist = mystring.split(" ")
+print(newlist)
