c0b923bb0114d0c818412e62907846b76ff5bba1
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-08-27 21:16:03 +0300

    add 1 to each element of list

--- _07_lists/lesson.py
+++ _07_lists/lesson.py
@@ -1,11 +1,17 @@
+#
 word = "49498588585"
 my_list = list(word)
 print(my_list)
 
+# соединить слова списка в строку
 mylist = ["anna", "25", "minsk"]
 mystring = " ".join(mylist)
 
 print(mystring)
 
+# разделить строку на слова разделителем
 newlist = mystring.split(" ")
 print(newlist)
+
+new_list = [5, "cherry", 0.4]
+print(type(new_list[1]))
---
+++ _09_for_loop_range/Lesson.py
@@ -0,0 +1,18 @@
+my_range = range(10)
+print(list(my_range))
+
+new_range = range(0,10,2)
+print(list(new_range))
+
+# reversed range
+rev_range = range(10, 0, -1)
+print(list(rev_range))
+
+# !!* add 1 to each element of list
+
+numbers = [9,10,11,12,13]
+
+for i in range(len(numbers)):
+    numbers[i] += 1
+
+print(numbers)
