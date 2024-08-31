f5304d5e54b5e9b8d42f6ad6161ddaa5cd75821f
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-08-31 11:52:22 +0300

    Cycle While - play_martingale() for 1 player

---
+++ _11_variables_scope/lesson11.py
@@ -0,0 +1,14 @@
+DEFAULT_LEVEL_EXPERIENCE = 200
+
+def is_leveled_up(*, current_experience:int, gained_experience: int) -> bool:
+    total_experience = current_experience + gained_experience
+    level_up = False
+
+    if total_experience >= DEFAULT_LEVEL_EXPERIENCE:
+        level_up = True
+
+    return level_up
+
+
+
+print(is_leveled_up(current_experience=160, gained_experience=70))
