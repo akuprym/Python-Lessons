21c1b56a4494e3e994f78f84c27db844b6ca9935
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-09-01 22:37:59 +0300

    Binance Api - requests.get(url, params={})

--- _18_requests/lesson18.py
+++ _18_requests/lesson18.py
@@ -1,18 +1,32 @@
 # in terminal: pip install requests
 
 import requests
+import time
 
 url = 'https://api.binance.com/api/v3/ticker/price'
 
-response = requests.get(url, params={'symbol': 'BTCUSDT'})
-content = response.content
+# response = requests.get(url, params={'symbol': 'BTCUSDT'})
+# content = response.content
 
-print(content)
-print(type(content))
-print('--------------')
+# print(content)  # Output: b'{"symbol":"BTCUSDT","price":"57749.05000000"}'
+# print(type(content))  # bytes
 
-price_object = response.json()
-price = float(price_object['price'])
+# price_object = response.json()
+# price = float(price_object['price'])
 
-print(price)
-print(type(price))
+# print(price)
+# print(type(price))
+
+bitcoin_prices = []
+
+for i in range(30):
+    response = requests.get(url, params={'symbol': 'BTCUSDT'})
+    price_object = response.json()
+    price = float(price_object['price'])
+    bitcoin_prices.append(price)
+    time.sleep(1)
+
+print(bitcoin_prices)
+print(len(bitcoin_prices))
+print(max(bitcoin_prices))
+print(min(bitcoin_prices))
