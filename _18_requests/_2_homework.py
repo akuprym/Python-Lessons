2a7535fafe5ee9846b256fa152dc35a8b309cb67
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-09-02 17:09:49 +0300

    requests - homework

--- _18_requests/_2_homework.py
+++ _18_requests/_2_homework.py
@@ -3,6 +3,24 @@
 #  2. Изучите структуру ответа, сравните её с ответом в запросе без params.
 #  3. Напишите код, который найдёт курс Etherium'а к доллару (ETHUSDT) в полученной из запроса структуре.
 
+import requests
+
+url = 'https://api.binance.com/api/v3/ticker/price'
+
+response = requests.get(url)
+
+for item in response.json():
+    if item['symbol'] == "ETHUSDT":
+        print(item['price'])
+
+
+
+
+
+
+
+
+
 #  решение
 
 import requests
@@ -11,4 +29,5 @@ response = requests.get('https://api.binance.com/api/v3/ticker/price')
 
 for ticker in response.json():
     if ticker['symbol'] == 'ETHUSDT':
-        print(ticker['price'])
+        pass
+        # print(ticker['price'])
