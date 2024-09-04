a805b9ec8d7ce8bfdc72c7de6b0e6f388af5bb0c
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-09-04 12:37:34 +0300

    added "try except" to binance api request

--- _18_requests/lesson18.py
+++ _18_requests/lesson18.py
@@ -5,18 +5,23 @@ import time
 
 url = 'https://api.binance.com/api/v3/ticker/price'
 
-# response = requests.get(url, params={'symbol': 'BTCUSDT'})
-# content = response.content
+try:
+    response = requests.get(url, params={'symbol': 'BTCUSDT'})
+    content = response.content
 
-# print(content)  # Output: b'{"symbol":"BTCUSDT","price":"57749.05000000"}'
-# print(type(content))  # bytes
+    print(content)  # Output: b'{"symbol":"BTCUSDT","price":"57749.05000000"}'
+    print(type(content))  # bytes
 
-# price_object = response.json()
-# price = float(price_object['price'])
+    price_object = response.json()
+    price = float(price_object['price'])
 
-# print(price)
-# print(type(price))
+    print(price)
+    print(type(price))
 
+except requests.exceptions.ConnectionError as error:
+    print(f"Error occured: {error}")
+
+"""
 bitcoin_prices = []
 
 for i in range(30):
@@ -30,3 +35,4 @@ print(bitcoin_prices)
 print(len(bitcoin_prices))
 print(max(bitcoin_prices))
 print(min(bitcoin_prices))
+"""
