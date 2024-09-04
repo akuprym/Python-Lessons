55790d61ef5bfaa18809624f721bd47e65e2ea97
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-09-04 20:09:43 +0300

    def __str__(self) -> to print Class

---
+++ _24_classes/lesson24.py
@@ -0,0 +1,17 @@
+
+class Ork:
+    def __init__(self, level: int) -> None:
+        self.level = level
+        self.attack_power = level * 100
+        self.health_points = level * 100
+
+    def atack(self):
+        print(f"Ork attacks with {ork.attack_power} power")
+
+# IMPORTANT! How to print Ork class:
+    def __str__(self):
+        return f"Ork with level: {self.level}, health points: {self.health_points}"
+
+ork = Ork(level=5)
+ork.atack()
+print(ork)
