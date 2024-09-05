d7d1e62b065ad6b347617799600b79c65aeb3d9b
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-09-05 17:47:25 +0300

    homework - add func level_up() to game

--- _24_classes/_5_homework.py
+++ _24_classes/_5_homework.py
@@ -9,6 +9,91 @@
 # Ork is alive Ork (level: 2, hp: 140)
 # Elf is dead Elf (level: 1, hp: -4)
 
+class Character:
+    def __init__(self, level: int):
+        self.level = level
+        self.attack_power = self.base_attack_power * level
+        self.health_points = self.base_health_points * level
+
+    def atack(self, *, target: "Character"):
+        target.got_damage(damage=self.attack_power)
+
+    def got_damage(self, *, damage: int) -> None:
+        damage = damage * (100 - self.defence) / 100
+        damage = round(damage)
+        self.health_points -= damage
+
+    def is_alive(self) -> bool:
+        return  self.health_points > 0
+
+    @property
+    def defence(self) -> int:
+        defence = self.base_defence * self.level
+        return defence
+
+    @ property
+    def max_health_points(self) -> int:
+        return self.base_health_points * self.level
+
+    def health_points_percent(self):
+        return 100 * self.health_points / self.max_health_points
+
+    def level_up(self):
+         if self.level < 4:
+             self.level += 1
+             self.health_points += int(self.max_health_points / 2)
+
+    def __str__(self):
+        return (f"{self.character_name} (level: {self.level}, health points: {self.health_points})")
+
+
+class Ork(Character):
+    base_attack_power = 10
+    base_health_points = 100
+    base_defence = 15
+    character_name = "Ork"
+
+    @property
+    def defence(self) -> int:
+        defence = super().defence
+        if self.health_points < 50:
+            defence *= 3
+        return defence
+
+
+class Elf(Character):
+    base_attack_power = 15
+    base_health_points = 50
+    base_defence = 10
+    character_name = "Elf"
+
+    def attack(self, *, target: "Character") -> None:
+        attack_power = self.attack_power
+        if target.health_points_percent() < 30:
+            attack_power = self.attack_power * 3
+        print(f"Elf's attack power: {attack_power}")
+        target.got_damage(damage=attack_power)
+
+
+def fight(*, character_1: Character, character_2: Character) -> None:
+    while character_1.is_alive() and character_2.is_alive():
+        character_1.atack(target=character_2)
+        if character_2.is_alive():
+            character_2.atack(target=character_1)
+    if character_1.is_alive():
+            character_1.level_up()
+    else:
+        character_2.level_up()
+
+
+    print(f"Character 1: {character_1}. is alive: {character_1.is_alive()}")
+    print(f"Character 2: {character_2}, is alive: {character_2.is_alive()}")
+
+
+ork = Ork(level=1)
+elf = Elf(level=1)
+
+fight(character_1=ork, character_2=elf)
 
 
 
@@ -28,8 +113,7 @@
 
 
 
-
-
+"""
 # решение (классы очень вариативны, ваше решение может отличаться)
 
 class Character:
@@ -123,3 +207,5 @@ def fight(*, character_1: Character, character_2: Character) -> None:
 
 
 fight(character_1=ork, character_2=elf)
+
+"""
